0e73145ecd3942a90e569dce2268e59cd62bd08d
Ioannis Antoniou <johnantony24@hotmail.com>
2022-12-18 18:01:48 +0100

    d2e2

---
+++ Javascripts/d2ex2.js
@@ -0,0 +1,3 @@
+const quote = 'There is no exercise better for the heart than reaching down and lifting people up.'
+const author = "John Holmes"
+console.log(`The quote ${quote} by ${author} teaches us to help one another.`)
