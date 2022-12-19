7256b139af1568802dcf04f11f3641292667962b
Ioannis Antoniou <johnantony24@hotmail.com>
2022-12-19 23:18:24 +0100

    d4e1

---
+++ Javascripts/d4e1.js
@@ -0,0 +1,13 @@
+/* Enter your age: 30
+You are old enough to drive.
+
+Enter your age:15
+You are left with 3 years to drive.*/
+
+const age = prompt('Enter your age: ')
+const diff = 18 - diff
+if (age >= 18){
+  alert("You are old enough to drive.")
+}else{
+  alert(`You are left with ${diff} years to drive`)
+}
