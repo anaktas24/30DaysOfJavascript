34bcf009d250ae099de028f3d8f0a5cab05c3ac4
Ioannis Antoniou <johnantony24@hotmail.com>
2022-12-20 18:18:23 +0100

    d5e2

---
+++ Javascripts/d5e1.6.js
@@ -0,0 +1,15 @@
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(text.split(' '))
+console.log(text.join)
+console.log(text.length)
---
+++ Javascripts/d5e1.js
@@ -0,0 +1,19 @@
+const arr = []
+const arr2 = [1,'Bdstreet',32, true, 'car']
+console.log(arr2.length)
+console.log(arr2[0])
+console.log(arr2[2])
+console.log(arr2[4])
+console.log('------------------')
+const mixedDataTypes = [2,'Bdstreet',32, 'true', 'car']
+console.log(mixedDataTypes.length)
+console.log('------------------')
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+console.log('------------------')
+console.log(itCompanies.length)
+console.log(itCompanies[0])
+console.log(itCompanies[3])
+console.log(itCompanies[6])
+console.log(itCompanies.join())
+console.log(itCompanies.includes('Facebook'))
+console.log(itCompanies.pop())
