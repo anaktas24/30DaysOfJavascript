db81db9644ffe510f6d7972487e0227c63519332
Ioannis Antoniou <johnantony24@hotmail.com>
2023-01-04 15:28:14 +0100

    day19

---
+++ Javascripts/d19e1,2.js
@@ -0,0 +1,36 @@
+//Create a closure which has one inner function
+function outerFunction() {
+  let count = 0;
+  function innerFunction() {
+      count++
+      return count
+  }
+
+  return innerFunction
+}
+const innerFunc = outerFunction()
+
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
+//Create a closure which has three inner functions
+function outerFunction() {
+  let count = 0;
+  function plusOne() {
+      count++
+      return count
+  }
+  function minusOne() {
+      count--
+      return count
+  }
+
+  return {
+      plusOne:plusOne(),
+      minusOne:minusOne()
+  }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
